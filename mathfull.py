744db863907d8c456809a25bb4d1ed08a0599def
Akash227788 <35484377+Akash227788@users.noreply.github.com>
2018-01-16 15:25:46 +0530

    Add files via upload

---
+++ mathfull.py
@@ -0,0 +1,13 @@
+a = 10
+b = 30
+sub = a - b
+m = a * b
+add = a + b
+div = a / b
+
+print ( " addition , subtraction , division and multiplication  of " , a , " and " , b , " is " , add , sub , div , m )
+
+
+
+
+
